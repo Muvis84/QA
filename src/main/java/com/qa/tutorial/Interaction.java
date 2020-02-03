d255e0ac645d15dcbddf9f0a83509c16d80880f9
Muvis84 <57877755+Muvis84@users.noreply.github.com>
2020-02-03 14:23:43 +0000

    first commit

---
+++ src/main/java/com/qa/tutorial/Interaction.java
@@ -0,0 +1,24 @@
+package com.qa.tutorial;
+
+public class Interaction {
+	
+	/**
+	 * this method is for you like
+	 * @return
+	 */
+	public String greeting() {
+		return "hi friend";
+	}
+	/**this method is for people in your  class
+	 * 
+	 * @return
+	 */
+
+	public String insult() {
+		return "Is trash";
+		
+	}
+
+}
+
+
